ce187c10982e43b53652862670567b133babfe39
jaiovi <sebastianjo2011@gmail.com>
2022-12-26 15:01:26 -0600

    Initial Commit

---
+++ PrimeraAppAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PrimeraAppAR
+//
+//  Created by Sebastián Jaiovi on 26/12/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
